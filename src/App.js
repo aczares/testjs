f23010949ba4cd54b71aa4e9e0c32ee6ecda0264
aleisley <anthon.asares@gmail.com>
2021-08-22 20:48:22 +0800

    based

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import './App.css';
 import { div } from 'prelude-ls';
 
@@ -26,14 +26,19 @@ import { div } from 'prelude-ls';
 
 const App = () => {
   const [num, setNum] = useState(0);
-  const clickHandler = async (num) => {
+  const [isSending, setIsSending] = useState(false);
+  const clickHandler = useCallback(async () => {
+    if (isSending) return;
+    setIsSending(true);
     const response = await fetch(`http://10.0.0.22?number=${num}`);
-    setNum(response.number);
-  };
+    const json = await response.json();
+    setNum(json.number);
+    setIsSending(false);
+  }, [num, isSending]);
   return (
     <div>
       <h1>The number is currently: {num}</h1>
-      <button onClick={() => clickHandler(num)}>Increase</button>
+      <button onClick={clickHandler} disabled={isSending}>Increase</button>
     </div>
   )
 };
