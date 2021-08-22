471b5e012274c24b56ee9998a82250b0bf7e8845
aleisley <anthon.asares@gmail.com>
2021-08-22 19:54:52 +0800

    New code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
+import { useState } from 'React';
 import './App.css';
+import { div } from 'prelude-ls';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+const App = () => {
+  const [num, setNum] = useState(0);
+  const clickHandler = async (num) => {
+    const response = await fetch(`http://10.0.0.22?number=${num}`);
+    setNum(response.number);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The number is currently: {num}</h1>
+      <button onClick={clickHandler}>Increase</button>
     </div>
-  );
-}
+  )
+};
 
 export default App;
